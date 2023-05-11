9c87ac3919b18fa3bad55e13d63836dc81c8fa59
Martin Chalupa <martichalupa@gmail.com>
2023-05-11 10:05:48 +0200

    Data

---
+++ src/app/data.tsx
@@ -0,0 +1,46 @@
+const alldata = [
+  {
+    id:1,
+    date: "April 4, 2017 – 2:17PM",
+    title: "How to get angry",
+    mentor: "Tom Watts",
+    description: "I will show you how in a second",
+  },
+  {
+    id:2,
+    date: "April 4, 2017 – 2:17PM",
+    title: "Mexican party vol.2",
+    mentor: "Matilda Daniels",
+    description: "Party in Scrollbar",
+  },
+  {
+    id:3,
+    date: "April 4, 2017 – 2:17PM",
+    title: "How to become Dark Soldier",
+    mentor: "Bill Soto",
+    description: "I will tell you insights about how I became Dark Soldier",
+  },
+  {
+    id:4,
+    date: "April 4, 2017 – 2:17PM",
+    title: "Parkour lesson",
+    mentor: "Johnny Erickson",
+    description: "Meet me at 5th Eve!",
+  },
+  {
+    id:5,
+    date: "April 4, 2017 – 2:17 PM",
+    title: "Party in Asgard",
+    mentor: "Ivan Wong",
+    description: "You can bring your +1!",
+  },
+  {
+    id:6,
+    date: "April 4, 2017 – 2:17 PM",
+    title: "Russian lesson",
+    mentor: "Herman Ray",
+    description: "Speak russian fluently.",
+  },
+];
+
+export default alldata;
---
+++ src/componens/Navg.tsx
@@ -0,0 +1,9 @@
+const Navg = () => {
+  return (
+    <>
+      <h1>Ahoj, to jsem ja</h1>
+    </>
+  );
+};
+
+export default Navg;
