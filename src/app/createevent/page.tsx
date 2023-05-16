06e579f2d4c5a56d747ecd361ca5e425478d4837
Martin Chalupa <martichalupa@gmail.com>
2023-05-16 14:44:21 +0200

    p

--- src/app/createevent/page.tsx
+++ src/app/createevent/page.tsx
@@ -1,17 +1,11 @@
 "use client";
+import Link from "next/link";
 import { use, useState } from "react";
 import style from "./page.module.css";
 import { Logo } from "@/componens/svg/Logo";
 import { X } from "./svg/X";
 
 const page = () => {
- const [Title, setTitle] = useState();
- const [Description, setDescription] = useState();
- const [Time, setTime] = useState();
- const [Capacity, setCapacity] = useState();
- 
-
- 
   return (
     <>
       <div className={style.all}>
@@ -20,8 +14,8 @@ const page = () => {
             <Logo></Logo>
           </p>
           <div className={style.close}>
-            <p className={style.X}>X</p>
-            <p className={style.CloseL}>Close</p>
+            <Link href="" className={style.X}>X</Link>
+            <Link href ="" className={style.CloseL}>Close</Link>
           </div>
         </div>
         <div className={style.allbox}>
@@ -34,16 +28,12 @@ const page = () => {
                 name="fname"
                 placeholder="Title"
                 className={style.oneform}
-                value={Title}
-                onChange={(e) => setTitle(e.target.value)}
               ></input>
               <input
                 type="text"
                 name="fname"
                 placeholder="Description"
                 className={style.oneform}
-                value={Description}
-                onChange={(e) => setDescription(e.target.value)}
               ></input>
               <input
                 name="fname"
@@ -55,16 +45,12 @@ const page = () => {
                 name="fname"
                 placeholder="Time"
                 className={style.oneform}
-                value={Time}
-                onChange={(e) => setTime(e.target.value)}
               ></input>
               <input
                 type="text"
                 name="fname"
                 placeholder="Capacity"
                 className={style.oneform}
-                value={Capacity}
-                onChange={(e) => setCapacity(e.target.value)}
               ></input>
             </div>
             <button className={style.createbtn}>CREATE NEW EVENT</button>
