c36c6c524258b7e64f73528644a199e9c291b775
pchalupa <chalupa.petr93@gmail.com>
2024-02-18 20:25:33 +0100

    refactor: register

--- src/app/Context/auth/index.tsx
+++ src/app/Context/auth/index.tsx
@@ -2,10 +2,11 @@
 
 import {
     User as FirebaseAuthUser,
+    createUserWithEmailAndPassword,
     onAuthStateChanged,
     signInWithEmailAndPassword,
 } from 'firebase/auth';
-import { doc, getDoc } from 'firebase/firestore';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 
 import { auth } from '@/services/firebase/auth';
@@ -18,6 +19,12 @@ export type User = { uid: string; fname: string; lname: string; email: string };
 type AuthContextType = {
     user: User | undefined;
     login: (email: string, password: string) => Promise<FirebaseAuthUser>;
+    register: (registerData: {
+        email: string;
+        password: string;
+        firstName: string;
+        lastName: string;
+    }) => Promise<FirebaseAuthUser>;
 };
 
 export const AuthContext = createContext<AuthContextType | undefined>(undefined);
@@ -33,6 +40,33 @@ export const AuthContextProvider: React.FC<{ children: React.ReactNode }> = ({ c
 
         return response.user;
     }, []);
+
+    const register = useCallback(
+        async ({
+            firstName,
+            lastName,
+            email,
+            password,
+        }: {
+            email: string;
+            password: string;
+            firstName: string;
+            lastName: string;
+        }) => {
+            const { user } = await createUserWithEmailAndPassword(auth, email, password);
+            const { token: idToken, expirationTime } = await user.getIdTokenResult();
+
+            await setDoc(doc(db, 'users', user.uid), {
+                fname: firstName,
+                lname: lastName,
+                email,
+            });
+            setAuthCookie(idToken, expirationTime);
+
+            return user;
+        },
+        [],
+    );
 
     const fetchUser = async (uid: string): Promise<User | undefined> => {
         try {
@@ -71,6 +105,7 @@ export const AuthContextProvider: React.FC<{ children: React.ReactNode }> = ({ c
 
     const contextValue: AuthContextType = {
         login,
+        register,
         user,
     };
     return <AuthContext.Provider value={contextValue}>{children}</AuthContext.Provider>;
